2d04578bb2311cd006a8db38a0229a3df77a6af1
euro20179 <sandollar2020@gmail.com>
2024-09-06 21:42:08 -0700

    fix(provider:anilist): gets media specific metadata wrong with manga

--- metadata/anilist.go
+++ metadata/anilist.go
@@ -42,9 +42,9 @@ type AnlistMediaEntry struct {
 
 	Description string `json:"description"`
 
-	Duration   uint  `json:"duration"`
-	Episodes   uint  `json:"episodes"`
-	StartDate struct  {
+	Duration  uint `json:"duration"`
+	Episodes  uint `json:"episodes"`
+	StartDate struct {
 		Year int `json:"year"`
 	} `json:"startDate"`
 
@@ -115,6 +115,31 @@ func mkAnilistRequest[T any, TOut any](anilistQuery AnilistQuery[T]) (TOut, erro
 	return *jData, nil
 }
 
+func applyManga(anilistData AnlistMediaEntry) (db.MetadataEntry, error) {
+	var o db.MetadataEntry
+	out := anilistData
+
+	mediaDependant := make(map[string]string)
+	mediaDependant["Manga-volumes"] = fmt.Sprintf("%d", out.Volumes)
+	mdString, _ := json.Marshal(mediaDependant)
+
+	o.MediaDependant = string(mdString)
+
+	o.Title = out.Title.English
+	o.Native_Title = out.Title.Native
+	if out.CoverImage.Medium != "" {
+		o.Thumbnail = out.CoverImage.Medium
+	} else {
+		o.Thumbnail = out.CoverImage.Large
+	}
+	o.ReleaseYear = int64(out.StartDate.Year)
+	o.Description = out.Description
+	o.ItemId = out.Id
+	o.Rating = float64(out.AverageScore)
+
+	return o, nil
+}
+
 func AnilistManga(entry *db.InfoEntry, metadataEntry *db.MetadataEntry) (db.MetadataEntry, error) {
 	var o db.MetadataEntry
 	searchTitle := entry.En_Title
@@ -141,23 +166,47 @@ func AnilistManga(entry *db.InfoEntry, metadataEntry *db.MetadataEntry) (db.Meta
 		return o, err
 	}
 
-	out := jData.Data.Media
+	return applyManga(jData.Data.Media)
+}
 
+func applyShow(aniInfo AnlistMediaEntry) (db.MetadataEntry, error) {
+	var outMeta db.MetadataEntry
 	mediaDependant := make(map[string]string)
-	mediaDependant["Manga-volumes"] = fmt.Sprintf("%d", out.Volumes)
+
+	var ty string
+	switch aniInfo.Format {
+	case "MOVIE":
+		ty = "Movie"
+	case "MANGA":
+		ty = "Manga"
+	case "SHOW":
+		ty = "Show"
+	}
+
+	mediaDependant[fmt.Sprintf("%s-episodes", ty)] = strconv.Itoa(int(aniInfo.Episodes))
+	mediaDependant[fmt.Sprintf("%s-episode-duration", ty)] = strconv.Itoa(int(aniInfo.Duration))
+	mediaDependant[fmt.Sprintf("%s-length", ty)] = strconv.Itoa(int(aniInfo.Episodes) * int(aniInfo.Duration))
+	mediaDependant[fmt.Sprintf("%s-airing-status", ty)] = aniInfo.Status
+
 	mdString, _ := json.Marshal(mediaDependant)
 
-	o.MediaDependant = string(mdString)
+	println(aniInfo.Title.Native, aniInfo.Title.English)
 
-	o.Title = out.Title.English
-	o.Native_Title = out.Title.Native
-	o.Thumbnail = out.CoverImage.Medium
-	o.ReleaseYear = int64(out.StartDate.Year)
-	o.Description = out.Description
-	o.ItemId = out.Id
-	o.Rating = float64(out.AverageScore)
+	if aniInfo.Title.Native != "" {
+		outMeta.Native_Title = aniInfo.Title.Native
+	}
+	if aniInfo.Title.English != "" {
+		outMeta.Title = aniInfo.Title.English
+	} else if aniInfo.Title.Romaji != "" {
+		outMeta.Title = aniInfo.Title.Romaji
+	}
+	outMeta.Thumbnail = aniInfo.CoverImage.Large
+	outMeta.Rating = float64(aniInfo.AverageScore)
+	outMeta.Description = aniInfo.Description
+	outMeta.MediaDependant = string(mdString)
+	outMeta.ReleaseYear = int64(aniInfo.StartDate.Year)
 
-	return o, nil
+	return outMeta, nil
 }
 
 func AnilistShow(entry *db.InfoEntry, metadataEntry *db.MetadataEntry) (db.MetadataEntry, error) {
@@ -187,33 +236,7 @@ func AnilistShow(entry *db.InfoEntry, metadataEntry *db.MetadataEntry) (db.Metad
 	}
 
 	out := jData.Data.Media
-
-	mediaDependant := make(map[string]string)
-
-	mediaDependant[fmt.Sprintf("%s-episodes", entry.Type)] = strconv.Itoa(int(out.Episodes))
-	mediaDependant[fmt.Sprintf("%s-episode-duration", entry.Type)] = strconv.Itoa(int(out.Duration))
-	mediaDependant[fmt.Sprintf("%s-length", entry.Type)] = strconv.Itoa(int(out.Episodes) * int(out.Duration))
-	mediaDependant[fmt.Sprintf("%s-airing-status", entry.Type)] = out.Status
-
-	mdString, _ := json.Marshal(mediaDependant)
-
-	println(out.Title.Native, out.Title.English)
-
-	if out.Title.Native != "" {
-		outMeta.Native_Title = out.Title.Native
-	}
-	if out.Title.English != "" {
-		outMeta.Title = out.Title.English
-	} else if out.Title.Romaji != "" {
-		outMeta.Title = out.Title.Romaji
-	}
-	outMeta.Thumbnail = out.CoverImage.Large
-	outMeta.Rating = float64(out.AverageScore)
-	outMeta.Description = out.Description
-	outMeta.MediaDependant = string(mdString)
-	outMeta.ReleaseYear = int64(out.StartDate.Year)
-
-	return outMeta, nil
+	return applyShow(out)
 }
 
 func AnlistProvider(entry *db.InfoEntry, metadataEntry *db.MetadataEntry) (db.MetadataEntry, error) {
@@ -257,20 +280,19 @@ func AnilistIdentifier(info IdentifyMetadata) ([]db.MetadataEntry, error) {
 
 	for _, entry := range jData.Data.Page.Media {
 		var cur db.MetadataEntry
-		cur.Thumbnail = entry.CoverImage.Large
-		cur.Description = entry.Description
-		cur.Rating = float64(entry.AverageScore)
-		cur.ItemId = entry.Id
-		if entry.Title.Native != "" {
-			cur.Native_Title = entry.Title.Native
+		if entry.Type == "MANGA" {
+			cur, err = applyManga(entry)
+			if err != nil {
+				println(err.Error())
+				continue
+			}
+		} else {
+			cur, err = applyShow(entry)
+			if err != nil {
+				println(err.Error())
+				continue
+			}
 		}
-		if entry.Title.English != "" {
-			cur.Title = entry.Title.English
-		} else if entry.Title.Romaji != "" {
-			cur.Title = entry.Title.Romaji
-		}
-		cur.MediaDependant = "{\"provider\": \"anilist\"}"
-
 		outMeta = append(outMeta, cur)
 	}
 
@@ -298,7 +320,7 @@ func AnilistById(id string) (db.MetadataEntry, error) {
 		return outMeta, err
 	}
 	out := jData.Data.Media
-	mediaDependant := make(map[string]string)
+	// mediaDependant := make(map[string]string)
 
 	var ty string
 	switch out.Format {
@@ -310,16 +332,18 @@ func AnilistById(id string) (db.MetadataEntry, error) {
 		ty = "Show"
 	}
 
-	mediaDependant[fmt.Sprintf("%s-episodes", ty)] = strconv.Itoa(int(out.Episodes))
-	mediaDependant[fmt.Sprintf("%s-episode-duration", ty)] = strconv.Itoa(int(out.Duration))
-	mediaDependant[fmt.Sprintf("%s-length", ty)] = strconv.Itoa(int(out.Episodes) * int(out.Duration))
-	mediaDependant[fmt.Sprintf("%s-airing-status", ty)] = out.Status
-
-	mdString, _ := json.Marshal(mediaDependant)
-	outMeta.Thumbnail = out.CoverImage.Large
-	outMeta.Rating = float64(out.AverageScore)
-	outMeta.Description = out.Description
-	outMeta.MediaDependant = string(mdString)
-	outMeta.ReleaseYear = int64(out.StartDate.Year)
+	if ty == "Manga" {
+		outMeta, err = applyManga(out)
+		if err != nil {
+			println(err.Error())
+			return outMeta, err
+		}
+	} else {
+		outMeta, err = applyShow(out)
+		if err != nil {
+			println(err.Error())
+			return outMeta, err
+		}
+	}
 	return outMeta, nil
 }
