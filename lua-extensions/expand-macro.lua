d2ef43aa054b556ae64dadf60062a0c049cd1305
euro20179 <sandollar2020@gmail.com>
2024-11-02 20:36:53 -0700

    feat: status + type macros

--- lua-extensions/expand-macro.lua
+++ lua-extensions/expand-macro.lua
@@ -8,29 +8,57 @@ function Expand_macro(macro)
         asName2I[v] = k
     end
 
+    local mediaTypes = {
+        "Show",
+        "Movie",
+        "MovieShort",
+        "Game",
+        "BoardGame",
+        "Song",
+        "Book",
+        "Manga",
+        "Collection",
+        "Picture",
+        "Meme"
+    }
+
+    local statuses = {
+        "Viewing",
+        "Finished",
+        "Dropped",
+        "Paused",
+        "Planned",
+        "ReViewing"
+    }
+
     local basicMacros = {
         isAnime = string.format("(artStyle & %d == %d)", asName2I['Anime'], asName2I['Anime']),
         r = "userRating",
         R = "rating",
         t = "en_title",
         T = "title",
-        d = "description"
+        d = "description",
     }
+
+    for _, item in ipairs(mediaTypes) do
+        basicMacros[string.lower(item)] = '(type == "' .. item .. '")'
+    end
+
+    for _, item in ipairs(statuses) do
+        basicMacros[string.lower(item)] = '(status == "' .. item .. '")'
+    end
 
     if basicMacros[macro] ~= nil then
         return basicMacros[macro], ""
-
     elseif aio.hasprefix(macro, "s:") then
         return comp("status", '"' .. aio.title(string.sub(macro, 3)) .. '"'), ""
-
     elseif aio.hasprefix(macro, "t:") then
         return comp("type", '"' .. aio.title(string.sub(macro, 3)) .. '"'), ""
-
     elseif aio.hasprefix(macro, "a:") then
         local titledArg = aio.title(string.sub(macro, 3))
         local as_int = asName2I[titledArg]
         if as_int == nil then
-            return  "", "Invalid art style: " .. titledArg
+            return "", "Invalid art style: " .. titledArg
         end
         return string.format("(artStyle & %d == %d)", as_int, as_int), ""
     end
