e5f1db7b41b7cb4b673969fed480bf640af141ec
euro20179 <sandollar2020@gmail.com>
2025-02-07 21:58:10 -0800

    fix: children/copies do not appear in sidebar even when selected

--- webservice/www/new-ui/index.js
+++ webservice/www/new-ui/index.js
@@ -741,7 +741,6 @@ viewAllElem.addEventListener("change", e => {
 * @param {InfoEntry[]} entries
 */
 function renderSidebar(entries) {
-    entries = entries.filter(a => a.ParentId === 0n && a.CopyOf === 0n)
     if (viewAllElem.checked) {
         selectItemList(entries, mode)
     } else {
